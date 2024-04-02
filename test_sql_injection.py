534ce8fa2ec4f0143b5cad782429f309fab6691e
valerik23pv <valerik23pv@gmail.com>
2024-04-02 13:14:56 +0700

    new

--- test_sql_injection.py
+++ test_sql_injection.py
@@ -2,17 +2,13 @@ import sqlite3
 
 sqliteConnection = sqlite3.connect('twist.sqlite')
 cursor = sqliteConnection.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER,
-            login VARCHAR,
-            password VARCHAR
-);        ''')
-sqliteConnection.commit()
-
 login = input("input login ")
-query = "select login, password FROM users WHERE login ==  '" + login + "';"
+password = input("input password ")
+query = "select login, password FROM users WHERE login ==  '" + login + "' and password =='" + password + "';"
 print(query)
 cursor.execute(query)
 result = cursor.fetchall()
-print(result)
+if result != None and len(result) > 0:
+    print("Успешно!")
+else:
+    print('Не угадал пароль давай попробуем еще раз. Подсказка, я админ')
